75b518a4d375a754728941be6a8719420fb8c338
Lonn Holiday <lonn@ljholiday.com>
2026-02-03 01:49:21 +0000

    Ignore instance config only; keep example config tracked

---
+++ config/config.example.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    // Absolute path to the directory containing markdown files
+    'repos_path' => '/absolute/path/to/pages',
+
+    // Password for accessing mdeditorlite
+    'password'   => 'change-me',
+];
+
